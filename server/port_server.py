c59f6f36d494ae931e77e56c4f72243bc12c4570
Joshua Herman <me@joshuaherman.org>
2016-11-20 10:42:41 -0800

    final -need fixing

--- server/port_server.py
+++ server/port_server.py
@@ -47,49 +47,54 @@ def signin():
   try:
     email = request.form.get('email', default=None, type=str)
     passwd = request.form.get('password', default=None, type=str)
-    cursor.execute("Select idUser from portiq.Users WHERE email = \'{0}\' and password = \'{1}\'".format(email, passwd))
+    cursor.execute("Select idUser from portiq.Users WHERE email = \'{0}\' and password = \'{1}\';".format(email, passwd))
     UID = cursor.fetchone()
     return json.dumps({'UID': UID})
   except Exception as e:
     return(str(e))
 
+
 @app.route('/getSchedule', methods=['POST'])
-#@cross_origin()
 def getSchedule():
+  cursor = mysql.connect().cursor()
   try:
-    cursor = mysql.connect().cursor()
-    CID = request.form.get('company', default=None, type=str)
-    Date = request.form.get('date', default=None, type=str)
-    port = request.form.get('port', default=None, type=str)
-
-    if Date != None:
-      cursor.execute("select Unload_Port, Container, Estimated_Vessel_Arrival from portiq.data WHERE Estimated_Vessel_Arrival =\'{}\';".format(date))
-      data = cursor.fetchall()
-
-    if port != None:
-      cursor.execute("select Container, Estimated_Vessel_Arrival, Estimated_Ship_Departure from portiq.data WHERE Unload_Port Like\'{}\';".format(port))
-      data = cursor.fetchall()
+    date = request.form.get('date')
+    cursor.execute("select * from portiq.data WHERE Estimated_Vessel_Arrival LIKE \"{}\";".format(date))
+    data = cursor.fetchall()
     return json.dumps({'data': data})
   except Exception as e:
     return(str(e))
 
 
-
 @app.route('/getPorts', methods=['POST'])
 #@cross_origin()
 def getPorts():
-  port = request.form.get('port', default=None, type=str)
-  return json.dumps({'data': port})
-
+  cursor = mysql.connect().cursor()
+  try:
+    port = request.form.get('port', default=None, type=str)
+    cursor.execute("select Container, Estimated_Vessel_Arrival from portiq.data WHERE Unload_Port =\'{}\';".format(port))
+    data = cursor.fetchall()
+    return json.dumps({'data': data})
+  except Exception as e:
+    return(str(e))
 
 @app.route('/getData')
 #@cross_origin()
 def getExampleData():
   cursor = mysql.connect().cursor()
-  cursor.execute("select * from data limit 1")
+  cursor.execute("select * from data limit 10")
   data = cursor.fetchall()
   return json.dumps({'data': data}, default=decimal_default)
 
+@app.route('/setBOL', methods=['POST'])
+def setBOL():
+  try:
+    cursor = mysql.connect().cursor()
+    CID = request.form.get('company', default=None, type=str)
+    BOL = request.form.get('BOL', default=None, type=str)
+    cursor.execute('INSERT INTO portiq.BillOfLaden (CompanyID, Bill_of_Lading) VALUES (\'{}\',\'{}\');'.format(CID, BOL))
+  except Exception as e:
+    return(str(e))
 
 
 if __name__ == "__main__":
